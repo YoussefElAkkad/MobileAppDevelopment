42ce9adb4c6aabfad8713c0a4b08959b8c8a3862
Youssef_Tamer_Ismail_Moustafa_ElAkkad <eng.yousseftamer2001@gmail.com>
2024-05-11 20:33:57 +0000

    session5_Content

---
+++ lib/To_do_list.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class To_Do_List extends StatelessWidget {
+  const To_Do_List({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        titleTextStyle: const TextStyle(
+            color: Colors.white, fontSize: 30, fontWeight: FontWeight.bold),
+        backgroundColor: Colors.blue,
+        centerTitle: true,
+        leading: const Icon(Icons.menu),
+        title: const Text("To Do List"),
+        actions: [
+          IconButton(
+            onPressed: () {
+              print("Hello world");
+            },
+            icon: const Icon(Icons.add),
+          )
+        ],
+      ),
+    );
+  }
+}
